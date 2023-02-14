e73e7fe94e8885e16b273ee53e904f7368cb5a62
Christy Le <lechrsty@gmail.com>
2023-02-13 20:21:11 -0800

    models created, migrations completed

---
+++ levelupapi/models/Game.py
@@ -0,0 +1,14 @@
+from django.db import models
+from .GameType import GameType
+from .Gamer import Gamer
+
+
+class Game(models.Model):
+    
+    title = models.CharField(max_length=55)
+    description = models.CharField(max_length=500)
+    game_type = models.ForeignKey(GameType, on_delete=models.SET_NULL, null=True)
+    creator = models.ForeignKey(Gamer, on_delete=models.SET_NULL, null=True)
+    created_on = models.DateField(auto_now_add=True)
+    player_count = models.IntegerField()
+    skill_level = models.CharField(max_length=50)
---
+++ levelupapi/models/__init__.py
@@ -0,0 +1,5 @@
+from .Gamer import Gamer
+from .Event import Event
+from .Game import Game
+from .GameType import GameType
+from .GamerEvent import GamerEvent
